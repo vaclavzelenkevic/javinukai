cc90067f5c6113c987d110f7cb0b0327026ea818
rkukutis <kukutis.ricardas@gmail.com>
2024-01-30 14:42:32 +0200

    Initial message

---
+++ javinukai-back/src/main/java/lt/javinukai/javinukai/JavinukaiApplication.java
@@ -0,0 +1,13 @@
+package lt.javinukai.javinukai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavinukaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavinukaiApplication.class, args);
+	}
+
+}
---
+++ javinukai-back/src/test/java/lt/javinukai/javinukai/JavinukaiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.javinukai.javinukai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavinukaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
